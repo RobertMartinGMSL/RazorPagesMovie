98b120abadb7d143e068e989ab5e13789e922086
Rob Martin <robert.martin@gmsl.co.uk>
2019-08-22 16:44:39 +0100

    End of tutorial

---
+++ Models/Movie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$", 
+            ErrorMessage = "Please enter a genre less than 30 characters")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z0-9]+[a-zA-Z0-9""'\s-]*$", 
+            ErrorMessage = "Please enter valid rating; U, PG, 12U, 15 or 18")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
+    }
+}
